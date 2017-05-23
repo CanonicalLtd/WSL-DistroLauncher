b3ae17d5523251baafc3c9daf5ffc1d066af6e23
Dimitri John Ledkov <xnox@ubuntu.com>
2017-05-23 12:14:18 +0100

    Fix pkg.bat

--- pkg.bat
+++ pkg.bat
@@ -4,8 +4,8 @@
 
 call build clean rel
 
-set _OWNER=DistroOwner
-set _AppxName=MyDistro
+set _OWNER=Ubuntu
+set _AppxName=Ubuntu
 set _FilemapName=filemap.txt
 
 set _KITS=()
@@ -29,13 +29,13 @@ if %_KITS%==() (
 ) 
 :FOUND_KITS
 
-if not exist %_OWNER%.pfx (
+if not exist "%_OWNER%".pfx (
     echo "Generating some test certs/keys to test sideloading with."
     echo "This is going to prompt you for a name and password, and then that password again, and then the password again."
     echo "You can enter whatever you want, these should only be used for local testing."
     @rem Creates keys for local testing.
     @rem Note that "CN=%_OWNER%" needs to match the Publisher in your appxmanifest
-    %_KITS%\makecert -r -h 0 -n "CN=%_OWNER%" -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -pe -sv %_OWNER%.pvk %_OWNER%.cer
+    %_KITS%\makecert -r -h 0 -n "CN=23596F84-C3EA-4CD8-A7DF-550DCE37BCD0" -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -pe -sv %_OWNER%.pvk %_OWNER%.cer
     %_KITS%\pvk2pfx -pvk %_OWNER%.pvk -spc %_OWNER%.cer -pfx %_OWNER%.pfx
 
     echo "You will need to install that cert before you can sideload the appx. "
@@ -44,9 +44,12 @@ if not exist %_OWNER%.pfx (
     echo %_KITS%\CertMgr /add %_OWNER%.cer /s /r localMachine root 
 )
 
-%_KITS%\makeappx.exe pack /m %_AppxName%.appxmanifest /f %_FilemapName% /p %_AppxName%.appx
-%_KITS%\signtool sign /v /f %_OWNER%.pfx /fd SHA256 %_AppxName%.appx
+%_KITS%\makeappx.exe pack /m MyDistro.appxmanifest /f filemap.txt /p Ubuntu.appx
+%_KITS%\signtool sign /v /f "%_OWNER%".pfx /fd SHA256 Ubuntu.appx
+
+%_KITS%\makeappx.exe pack /m MyDistro.appxmanifest /f filemap-empty.txt /p UbuntuEmpty.appx
+%_KITS%\signtool sign /v /f "%_OWNER%".pfx /fd SHA256 UbuntuEmpty.appx
 
 
 @rem You will need to run this once before you can sideload your appx on a machine.
-@rem %_KITS%\CertMgr /add %_OWNER%.cer /s /r localMachine root 
+@rem %_KITS%\CertMgr /add %_OWNER%.cer /s /r localMachine root
