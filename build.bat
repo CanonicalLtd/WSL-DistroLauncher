fdc7ea556d77cc2d505bda60779620287194fc68
Balint Reczey <balint.reczey@canonical.com>
2018-03-27 17:26:23 +0100

    Generate temporary key for the build again

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set _KEY=DistroLauncher-Appx\\Ubuntu_TemporaryKey
+
 rem Add path to MSBuild Binaries
 set MSBUILD=()
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" (
@@ -34,6 +36,22 @@ if %MSBUILD%==() (
 :FOUND_MSBUILD
 set _MSBUILD_TARGET=Build
 set _MSBUILD_CONFIG=Debug
+
+if not exist "%_KEY%".pfx (
+    echo "Generating some test certs/keys to test sideloading with."
+    echo "This is going to prompt you for a name and password, and then that password again, and then the password again."
+    echo "You can enter whatever you want, these should only be used for local testing."
+    @rem Creates keys for local testing.
+    @rem Note that "CN=%_OWNER%" needs to match the Publisher in your appxmanifest
+    makecert -r -h 0 -n "CN=23596F84-C3EA-4CD8-A7DF-550DCE37BCD0" -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -pe -sv %_KEY%.pvk %_KEY%.cer
+    pvk2pfx -pvk %_KEY%.pvk -spc %_KEY%.cer -pfx %_KEY%.pfx
+
+    echo "You will need to install that cert before you can sideload the appx. "
+    echo "You can install it with the following command (in an admin window)"
+    echo
+    echo CertMgr /add %_KEY%.cer /s /r localMachine root
+)
+
 
 :ARGS_LOOP
 if (%1) == () goto :POST_ARGS_LOOP
