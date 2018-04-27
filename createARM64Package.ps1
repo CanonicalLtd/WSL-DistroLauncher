ba667436ecb266bf9eddc866486ff24706938278
Balint Reczey <balint.reczey@canonical.com>
2018-04-27 13:54:40 +0200

    Don't overwrite ARM64 appx when repacking it as faked arm one
    Also fix repacking ARM64 appx

--- createARM64Package.ps1
+++ createARM64Package.ps1
@@ -1,6 +1,7 @@
 # Update these two variables with the paths to the ARM64 appx and pfx file used to sign the app.
 $appxPath = $args[0]
 $pfxFile = $args[1]
+$outAppxPath = $args[2]
 
 # Replace arm64 processor architecture with arm to work around Microsoft Store ingestion issues.
 copy $appxPath .\archive.zip
@@ -8,8 +9,8 @@ Expand-Archive .\archive.zip -DestinationPath extracted -Force
 (Get-Content .\extracted\AppxManifest.xml).replace('ProcessorArchitecture="arm64"', 'ProcessorArchitecture="arm"') | Set-Content .\extracted\AppxManifest.xml
 
 # Create and sign the appx.
-makeappx pack /o /m extracted\AppxManifest.xml /f "DistroLauncher-Appx\ARM64\Release\filemap.map.txt" /p $appxPath
-signtool sign /v /f $pfxFile /fd SHA256 $appxPath
+makeappx pack /d extracted /p $outAppxPath
+signtool sign /v /f $pfxFile /fd SHA256 $outAppxPath
 
 #Cleanup
 Remove-Item .\archive.zip
