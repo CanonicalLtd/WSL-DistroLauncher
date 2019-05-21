9ca7255061c0e71d608f3a3ad344dbea0db1403f
Balint Reczey <balint.reczey@canonical.com>
2019-05-21 21:09:39 +0200

    Bump version to 1604.2019.521.0

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set _KEY=DistroLauncher-Appx\\DistroLauncher-Appx_TemporaryKey
-set VERSION=1604.2019.204.0
+set VERSION=1604.2019.521.0
 
 rem Add path to MSBuild Binaries
 set MSBUILD=()
